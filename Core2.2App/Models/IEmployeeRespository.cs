067555c6e333824736e4c97c0ab8bab6ed123a65
Trace Larson <TLARSON@dmione.com>
2019-09-26 11:25:08 -0500

    created models folder, Employee class with employee information, IEmployeeRepository with GetEmployee action, and MockEmployeeRepository with hardcoded employee data which also implementes IEmployeeRepository interface

---
+++ Core2.2App/Models/IEmployeeRespository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core2._2App.Models
+{
+    interface IEmployeeRespository
+    {
+        Employee GetEmployee(int Id);
+    }
+}

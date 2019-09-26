7c911ef64948fdeff5e416028749ce15a58c2e54
Trace Larson <TLARSON@dmione.com>
2019-09-26 16:19:04 -0500

    added /home/details action to the HomeController, details sends information about a model to a view (no views implemented as of yet)

--- Core2.2App/Controllers/HomeController.cs
+++ Core2.2App/Controllers/HomeController.cs
@@ -9,15 +9,23 @@ namespace Core2._2App.Controllers
 {
     public class HomeController : Controller
     {
-        private IEmployeeRespository _employeeRepository;
+        private readonly IEmployeeRespository _employeeRepository;
 
         public HomeController(IEmployeeRespository employeeRepository)
         {
             _employeeRepository = employeeRepository;
         }
+
         public string Index()
         {
             return _employeeRepository.GetEmployee(1).Name;
+        }
+            
+        public ViewResult Details()
+        {
+            Employee model = _employeeRepository.GetEmployee(1);
+            return View(model);
+ 
         }
     }
 }

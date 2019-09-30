783997a2232f71387b1fdbf3a8627889e0731392
Trace Larson <TLARSON@dmione.com>
2019-09-30 11:18:11 -0500

    added example (to be removed) of attribute based routing

--- Core2.2App/Controllers/HomeController.cs
+++ Core2.2App/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Core2._2App.Controllers
 {
+    [Route("[controller]/[action]")]
     public class HomeController : Controller
     {
         private readonly IEmployeeRepository _employeeRepository;
@@ -17,18 +18,21 @@ namespace Core2._2App.Controllers
             _employeeRepository = employeeRepository;
         }
 
+        [Route("~/Home")]
+        [Route("~/")]
         public ViewResult Index()
         {
             var model = _employeeRepository.GetAllEmployees();
             return View(model);
 
         }
-            
-        public ViewResult Details(int id)
+
+        [Route("{id?}")]
+        public ViewResult Details(int? id)
         {
             HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
             {
-                 Employee = _employeeRepository.GetEmployee(1),
+                 Employee = _employeeRepository.GetEmployee(id??1),
                  PageTitle =  "Employee Details"
             };
 
--- Core2.2App/Startup.cs
+++ Core2.2App/Startup.cs
@@ -38,7 +38,8 @@ namespace Core2._2App
 
             app.UseStaticFiles();
 //            app.UseMvcWithDefaultRoute();
-            app.UseMvc(routes => { routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}"); });
+//            app.UseMvc(routes => { routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}"); });
+            app.UseMvc();
         }
     }
 }
